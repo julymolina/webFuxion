b4eaa0dc18e38feded879781cfc5a6fee718c74a
Rene Arias <rene_811@hotmail.com>
2020-12-09 23:54:25 -0500

    add mixpanel ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
+declare var mixpanel: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'epigenetica';
+
+  ngAfterViewInit() {
+    mixpanel.track('AbrioApp');
+  }
 }
